773d8399b902f0184c46db5ffa34bd8938d571d5
a12d-tech <gntics@gmail.com>
2020-05-03 19:35:03 +0200

    remove hello component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { CytoscapeVisualizationComponent } from '../cytoscape-visualization/cytoscape-visualization.component';
 import { AlgorithmManagerComponent } from '../algorithm-manager/algorithm-manager.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CytoscapeVisualizationComponent, AlgorithmManagerComponent ],
+  declarations: [ AppComponent, CytoscapeVisualizationComponent, AlgorithmManagerComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
