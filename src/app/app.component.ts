49a8857c1c6faa59b221a206ecd408a4100d74a2
Alex Quard <gntics@gmail.com>
2020-05-17 17:59:48 +0200

    angular cli 9 new project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,10 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { Tree, TreeNode } from '../shared/tree';
-import { CytoscapeVisualizationComponent } from '../cytoscape-visualization/cytoscape-visualization.component';
-import { Subject } from 'rxjs';
-
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  public tree: Tree<string>;
-  public algorithm: Subject<Algorithm> = new Subject<Algorithm>();
-
-  constructor() { }
-
-  public ngOnInit(): void {
-    this.tree = this.buildTree();
-  }
-
-  public onAlgoSeledted(algo): void {
-    console.log('delegate algo to CytoscapeVisualizationComponent');
-    this.algorithm.next(algo);
-  }
-
-
-  private buildTree(): Tree<string> {
-    const tree = new Tree<string>();
-
-    const nodeA = new TreeNode<string>('a');
-    const nodeB = new TreeNode<string>('b');
-    const nodeC = new TreeNode<string>('c');
-    const nodeD = new TreeNode<string>('d');
-    const nodeE = new TreeNode<string>('e');
-
-    nodeA.addChildren(nodeB, nodeE);
-    nodeB.addChildren(nodeC, nodeE);
-    nodeC.addChildren(nodeD, nodeE);
-    nodeD.addChildren(nodeE);
-
-    return tree.addRoot(nodeA);
-  }
-
+export class AppComponent {
+  title = 'angular-node-graph';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CytoscapeVisualizationComponent } from '../cytoscape-visualization/cytoscape-visualization.component';
-import { AlgorithmManagerComponent } from '../algorithm-manager/algorithm-manager.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, CytoscapeVisualizationComponent, AlgorithmManagerComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
